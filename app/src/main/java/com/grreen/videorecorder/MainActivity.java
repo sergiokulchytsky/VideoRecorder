308141e50c4d5611dbf3006967ea13063f149e69
Darmaru <serhiyx16@gmail.com>
2015-12-24 00:19:35 +0200

    Write Main Class. Add Preview class, to get video preview in linear layout.

--- app/src/main/java/com/grreen/videorecorder/MainActivity.java
+++ app/src/main/java/com/grreen/videorecorder/MainActivity.java
@@ -1,13 +1,223 @@
 package com.grreen.videorecorder;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private Camera mCamera;
+    private Preview mPreview;
+    private MediaRecorder mediaRecorder;
+    private Button switchCamera;
+    private Context myContext;
+    private boolean cameraFront = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+        initialize();
+    }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        int numberOfCameras = Camera.getNumberOfCameras();
+
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                cameraId = i;
+                cameraFront = true;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        int numberOfCameras = Camera.getNumberOfCameras();
+
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                cameraFront = false;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!hasCamera(myContext)) {
+            Toast toast = Toast.makeText(myContext, "Sorry, your phone does not have a camera!", Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
+        if (mCamera == null) {
+            if (findFrontFacingCamera() < 0) {
+                Toast.makeText(this, "No front facing camera found.", Toast.LENGTH_LONG).show();
+                switchCamera.setVisibility(View.GONE);
+            }
+            mCamera = Camera.open(findBackFacingCamera());
+            mPreview.refreshCamera(mCamera);
+        }
+    }
+
+    public void initialize() {
+        LinearLayout cameraPreview = (LinearLayout) findViewById(R.id.camera_preview);
+
+        mPreview = new Preview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+
+        Button capture = (Button) findViewById(R.id.button_capture);
+        capture.setOnClickListener(captureListener);
+
+        switchCamera = (Button) findViewById(R.id.button_ChangeCamera);
+        switchCamera.setOnClickListener(switchCameraListener);
+    }
+
+    OnClickListener switchCameraListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (!recording) {
+                int camerasNumber = Camera.getNumberOfCameras();
+                if (camerasNumber > 1) {
+                    releaseCamera();
+                    chooseCamera();
+                } else {
+                    Toast toast = Toast.makeText(myContext, "Sorry, your phone has only one camera!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        }
+    };
+
+    public void chooseCamera() {
+        if (cameraFront) {
+            int cameraId = findBackFacingCamera();
+            if (cameraId >= 0) {
+                mCamera = Camera.open(cameraId);
+                mPreview.refreshCamera(mCamera);
+            }
+        } else {
+            int cameraId = findFrontFacingCamera();
+            if (cameraId >= 0) {
+                mCamera = Camera.open(cameraId);
+                mPreview.refreshCamera(mCamera);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private boolean hasCamera(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean recording = false;
+    OnClickListener captureListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (recording) {
+                mediaRecorder.stop();
+                releaseMediaRecorder();
+                Toast.makeText(MainActivity.this, "Video captured!", Toast.LENGTH_LONG).show();
+                recording = false;
+            } else {
+                if (!prepareMediaRecorder()) {
+                    Toast.makeText(MainActivity.this, "Fail in prepareMediaRecorder()!\n - Ended -", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        try {
+                            mediaRecorder.start();
+                        } catch (final Exception ex) {
+                            // Log.i("---","Exception in thread");
+                        }
+                    }
+                });
+
+                recording = true;
+            }
+        }
+    };
+
+    private void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            mCamera.lock();
+        }
+    }
+
+    private boolean prepareMediaRecorder() {
+
+        mediaRecorder = new MediaRecorder();
+
+        mCamera.unlock();
+        mediaRecorder.setCamera(mCamera);
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+
+        mediaRecorder.setOutputFile("/sdcard/myvideo.mp4");
+        mediaRecorder.setMaxDuration(600000);
+        mediaRecorder.setMaxFileSize(50000000);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.cancelAutoFocus();
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
---
+++ app/src/main/java/com/grreen/videorecorder/Preview.java
@@ -0,0 +1,67 @@
+package com.grreen.videorecorder;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Preview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public Preview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+
+            if (mCamera == null) {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            }
+        } catch (IOException e) {
+            Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void refreshCamera(Camera camera) {
+        if (mHolder.getSurface() == null) {
+            return;
+        }
+
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            //
+        }
+
+        setCamera(camera);
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        refreshCamera(mCamera);
+    }
+
+    public void setCamera(Camera camera) {
+        mCamera = camera;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
